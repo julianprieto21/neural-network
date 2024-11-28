13994942bbb351d7eeb1d9578443b346350b4673
Julian Prieto <prietojulian2003@gmail.com>
2024-11-27 23:06:40 -0300

    remove loss functions

--- training/loss.py
+++ training/loss.py
@@ -36,49 +36,6 @@ def categorical_cross_entropy(output: np.ndarray, target: np.ndarray, reduction:
         raise ValueError(f'Reduccion {reduction} no soportada')
     return np.round(loss, 3)
 
-def mean_squared_error(output: np.ndarray, target: np.ndarray, reduction: str='mean') -> float:
-    """
-    Calcula el loss de la red utilizando el método de error cuadrático medio
-
-    :param output: salida de la red
-    :param target: etiquetas reales
-    :param reduction: tipo de reducción. Por defecto 'mean'
-    :return loss: loss (pérdida) de la red
-    """
-    loss = np.mean((output - target) ** 2)
-    if reduction == 'mean':
-        loss = loss.mean()
-    elif reduction == 'sum':
-        loss = loss.sum()
-    elif reduction == 'none':
-        pass
-    else:
-        raise ValueError(f'Reduccion {reduction} no soportada')
-    return np.round(loss, 3)
-
-def categorical_mean_squared_error(output: np.ndarray, target: np.ndarray, reduction: str='mean') -> float:
-    """
-    Calcula el loss de la red utilizando el método de error cuadrático medio
-
-    :param output: salida de la red
-    :param target: etiquetas reales
-    :param reduction: tipo de reducción. Por defecto 'mean'
-    :return loss: loss (pérdida) de la red
-    """
-    #TODO: Revisar
-    epsilon = 1e-10
-    output = np.clip(output, epsilon, 1 - epsilon)
-    loss = np.mean(np.sum(target * np.log(output) + (1-target) * np.log(1-output), axis=1))
-    if reduction == 'mean':
-        loss = loss.mean()
-    elif reduction == 'sum':
-        loss = loss.sum()
-    elif reduction == 'none':
-        pass
-    else:
-        raise ValueError(f'Reduccion {reduction} no soportada')
-    return np.round(loss, 3)
-
 if __name__ == '__main__':
     y_true = np.array([[0, 1, 0], [0, 0, 1]])
     y_pred = np.array([[0.05, 0.95, 0], [0.1, 0.8, 0.1]])
