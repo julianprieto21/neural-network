8cfb17b27826224431ee9fe70c74efc9a2037a45
Julian Prieto <prietojulian2003@gmail.com>
2024-11-29 17:31:47 -0300

    Cambio de tipo de padding para pruebas iniciales

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from utils.metrics import accuracy
 import models.layers as layers
 from models.activations import ReLU, Softmax
 import numpy as np
+import random
+
+np.random.seed(21)
+random.seed(21)
 
 if __name__ == '__main__':
     model = NeuralNetwork(
         layers=(
-            layers.Conv2D(input_shape=(1, 28, 28), filters=4, filter_size=3, activation=ReLU(), padding='same'),
+            layers.Conv2D(input_shape=(1, 28, 28), filters=4, filter_size=3, activation=ReLU(), padding='valid'),
             layers.MaxPool2D(pool_size=3),
             # layers.Conv2D(filters=64, filter_size=3, activation=ReLU(), padding='same'),
             # layers.MaxPool2D(pool_size=2, stride=2),
@@ -21,8 +25,12 @@ if __name__ == '__main__':
         loss=cross_entropy, metrics=accuracy)
     
     model.compile()
-    # model.summary()
-    model._forward(np.random.rand(1, 28, 28))
-    # logits = model.forward(np.random.rand(1, 28, 28))
-    # pred = np.argmax(logits, axis=0)
-    # print(pred)
+    model.summary()
+    # model._forward(np.random.rand(1, 28, 28))
+    preds = model._forward(np.random.rand(1, 28, 28))
+    labels = np.random.randint(0, 10, size=(1, 10))
+    first_grads = preds - labels
+    grads = model._backward(first_grads)
+
+    # params = self.get_params()
+    # params = self.optimizer(params, grads)
