8f8d7331c301f287694de297cf8b0ba0b6622064
Julian Prieto <prietojulian2003@gmail.com>
2024-12-31 10:21:54 -0300

    cambios generales en clase de red neuronal

--- models/models.py
+++ models/models.py
@@ -19,46 +19,65 @@ class NeuralNetwork:
         self.metrics = metrics
         self.verbose = verbose
         self.params = None
+        self.data_grads = None
         self.compile()
+    
+    def compile(self) -> None:
+        """
+        Compila el modelo.
+        """
+        raise NotImplementedError
+
+    def log(self, text: str) -> None:
+        """
+        Escribe un mensaje en la consola
+
+        :param text: mensaje a escribir
+        """
+        if self.verbose:
+            print(text)
+class ConvolutionalNeuralNetwork(NeuralNetwork):
+    def __init__(self, layers: list[Layer], optimizer: Optimizer, loss: any, metrics: any, verbose: bool=True) -> None:
+        """
+        Constructor de un modelo de red neuronal
+
+        :param layers: capas de la red
+        :param optimizer: método de optimización
+        :param loss: función de pérdida
+        :param metrics: función de cálculo de métricas
+        :param verbose: indica si se debe mostrar mensajes de progreso
+        """
+        super().__init__(layers, optimizer, loss, metrics, verbose)
     
     def summary(self) -> None:
         """
         Muestra un resumen del modelo
         """
-        parameters = 0
+        parameters = sum(arr.size for arr in self.get_params())
         print(f'Modelo de red neuronal con {len(self.layers) + 1} capas:')
         print(f'- input: Entrada - {self.layers[0].input_shape}')
         for layer in self.layers:
-            layer_parameters = 0
             if layer.weights is not None: 
-                layer_parameters = layer.weights.size + layer.bias.size
-                parameters += layer_parameters
-            print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape} - {layer_parameters} params')
-            # if layer.weights is None: continue
-            # parameters += layer.weights.size
-            # parameters += layer.bias.size
-        print(f'Optimizador: {self.optimizer.__class__.__name__}')
+                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape} - {layer.weights.size + layer.bias.size} params')
+                print(f'    Pesos: ', layer.weights.shape)
+                print(f'    Bias: ', layer.bias.shape)
+            else:
+                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape}')
+        print(f'Optimizador: {self.optimizer.__class__.__name__} - Learning rate: {self.optimizer.learning_rate}')
         print(f'Función de pérdida: {self.loss.__name__}')        
         print(f'Función de cálculo de métricas: {self.metrics.__name__}')
         print(f'Cantidad de parámetros: {parameters}')
-        print('---')
-        for layer in self.layers:
-            if layer.weights is None: continue
-            print(layer.name)
-            print('Pesos: ', layer.weights.shape)
-            print('Bias: ', layer.bias.shape)
 
     def compile(self) -> None:
         """
-        Compila el modelo. Recupera los parámetros y genera las dimensiones de salida de las capas
+        Compila el modelo.
         """
-        first_layer = self.layers[0]
-        first_layer.compile()
-        output_shape = first_layer.output_shape
+        output_shape = self.layers[0].output_shape
         for layer in self.layers[1:]:
             layer.input_shape = output_shape
             layer.compile()
             output_shape = layer.output_shape
+        self.params = self.get_params()
 
     def get_params(self) -> list[np.ndarray]:
         """
@@ -86,7 +105,7 @@ class NeuralNetwork:
                 grads.append(layer.bias_grads)
         return grads
 
-    def _forward(self, data: np.ndarray) -> np.ndarray:
+    def __call__(self, data: np.ndarray) -> np.ndarray:
         """
         Realiza una propagación hacia adelante
 
@@ -94,7 +113,7 @@ class NeuralNetwork:
         :return y: tensor de salida
         """
         for layer in self.layers:
-            data = layer.forward(data)
+            data = layer(data)
         return data
     
     def _backward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
@@ -106,12 +125,12 @@ class NeuralNetwork:
         :return param_grads: gradientes de los parámetros
         """      
 
-        preds = self._forward(x)
+        preds = self(x)
         grad_output = preds - y 
         for layer in reversed(self.layers):
             grad_output = layer.backward(grad_output)
-        param_grads = self._get_grads()
-        return param_grads
+        self.data_grads = grad_output
+        return self._get_grads()
         
     def _update_params(self, param_grads: list[np.ndarray]) -> None:
         """
@@ -119,10 +138,7 @@ class NeuralNetwork:
 
         :param param_grads: gradientes de los parámetros	
         """
-        # Actualiza los parámetros del objeto del modelo
         self.params = self.optimizer(self.params, param_grads)
-
-        # Actualiza los parámetros del objeto de cada capa
         i = 0
         for layer in self.layers:
             if layer.weights is not None and layer.bias is not None:
@@ -145,31 +161,32 @@ class NeuralNetwork:
             metric_history = []
             self.log(f'Epoch {epoch + 1}/{epochs}')
             for batch in range(0, train_data.shape[0], batch_size):
-                batch_data = train_data[batch:batch+batch_size]
-                batch_labels = train_labels[batch:batch+batch_size]
+                x_batch = train_data[batch:batch+batch_size]
+                y_batch = train_labels[batch:batch+batch_size]
 
-                self.params = self.get_params()   
-                param_grads = [0 for _ in self.params]
-                for i in range(batch_data.shape[0]):
-                    grads = self._backward(batch_data[i, :, :], batch_labels[i])
-                    for j in range(len(grads)):
-                        param_grads[j] += grads[j]
-
-                for i in range(len(param_grads)):
-                    param_grads[i] /= batch_data.shape[0]
-                self._update_params(param_grads)
-                
-                loss, metric = self.evaluate(batch_data, batch_labels)
+                loss, metric = self.evaluate(x_batch, y_batch)
                 loss_history.append(loss)
                 metric_history.append(metric)
-                self.log(f'| Batch {batch//batch_size + 1}/{train_data.shape[0]//batch_size} - Loss: {loss:.4f} - Metric: {metric}')
 
-            self.log(f'Epoch {epoch + 1}/{epochs} finished - Loss: {np.mean(loss_history):.4f} - Metric: {np.mean(metric_history)}')
+                param_grads = [np.zeros_like(param) for param in self.params]
+                for i in range(x_batch.shape[0]):
+                    grads = self._backward(x_batch[i, :, :], y_batch[i])
+                    for j, grad in enumerate(grads):
+                        param_grads[j] += grad
+
+                param_grads = [
+                    np.where(abs(grad / x_batch.shape[0]) < 1e-07, 0, grad / x_batch.shape[0]) 
+                    for grad in param_grads
+                ]
+                self._update_params(param_grads)
+                
+                self.log(f'| Batch {batch//batch_size + 1}/{train_data.shape[0]//batch_size} - Loss: {np.mean(loss_history):.4f} - Metric: {np.mean(metric_history)}')
+
             if validation_data is not None:
                 loss, metric = self.evaluate(validation_data, validation_labels)
                 self.log(f'Validation - Loss: {loss:.4f} - Metric: {metric}')
 
-    def predict(self, test_data: np.ndarray, probs: bool=False) -> np.ndarray:
+    def predict(self, test_data: np.ndarray, probs: bool=True) -> np.ndarray:
         """
         Realiza una predicción sobre el modelo de red neuronal
 
@@ -177,7 +194,7 @@ class NeuralNetwork:
         :param probs: indica si se debe devolver las probabilidades de cada clase o la clase ganadora
         :return: matriz de predicciones
         """
-        preds = self._forward(test_data)
+        preds = self(test_data)
         return preds if probs else np.argmax(preds, axis=0)
     
     def evaluate(self, test_data: np.ndarray, test_labels: np.ndarray) -> float:
@@ -193,7 +210,6 @@ class NeuralNetwork:
         metric = 0
         for i in range(test_data.shape[0]):
             pred = self.predict(test_data[i, :, :], probs=True)
-            pred = np.array([pred])
             true = np.array([test_labels[i]])
             loss += self.loss(true, pred)
             metric += self.metrics(true, pred)
@@ -201,12 +217,3 @@ class NeuralNetwork:
         loss /= test_data.shape[0]
         metric /= test_data.shape[0]
         return round(loss, 3), round(metric, 3)
-
-    def log(self, text: str) -> None:
-        """
-        Escribe un mensaje en la consola
-
-        :param text: mensaje a escribir
-        """
-        if self.verbose:
-            print(text)
