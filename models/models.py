8b5b59a22f11d2b0286d6fcf0c173da94cc26643
Julian Prieto <prietojulian2003@gmail.com>
2025-01-08 15:50:57 -0300

    métodos de CNN llevados a clase padre para evitar repeticion de codigo. Agregado de clase RNN.

--- models/models.py
+++ models/models.py
@@ -3,9 +3,11 @@ from training.optimizers import Optimizer
 from .layers import Layer, Conv2D
 from training.losses import Loss
 import h5py
+from PIL import Image
+import matplotlib.pyplot as plt
 
 class NeuralNetwork:
-    def __init__(self, input_shape: tuple[int, ...], layers: list[Layer], optimizer: Optimizer, loss: Loss, metrics: any, verbose: bool=True) -> None:
+    def __init__(self, input_shape: tuple[int, ...], layers: list[Layer], optimizer: Optimizer, loss: Loss, metrics: any, verbose: bool=False) -> None:
         """
         Constructor de un modelo de red neuronal
 
@@ -24,12 +26,105 @@ class NeuralNetwork:
         self.verbose = verbose
         self.params = None
         self.compile()
+
+    def summary(self) -> None:
+        """
+        Muestra un resumen del modelo
+        """
+        parameters = sum(param.size for param in self.get_params())
+        print(f'{__class__.__name__} con {len(self.layers) + 1} capas:')
+        print(f'- input: Entrada - {self.input_shape}')
+        for layer in self.layers:
+            if hasattr(layer, 'weights') and hasattr(layer, 'bias'):
+                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape} - {layer.get_params().size} params')
+                print(f'    Pesos: ', layer.weights.shape)
+                if hasattr(layer, 'recurrent_weights'):
+                    print(f'    Pesos Recurrentes: ', layer.recurrent_weights.shape)
+                print(f'    Bias: ', layer.bias.shape)
+            else:
+                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape}')
+        print(f'Optimizador: {self.optimizer.__class__.__name__} - Learning rate: {self.optimizer.learning_rate}')
+        print(f'Función de pérdida: {self.loss.__class__.__name__}')        
+        print(f'Función de cálculo de métricas: {self.metrics.__name__}')
+        print(f'Cantidad de parámetros: {parameters}')
     
     def compile(self) -> None:
         """
         Compila el modelo.
         """
-        raise NotImplementedError
+        self.layers[0].compile(self.input_shape)
+        output_shape = self.layers[0].output_shape
+        for layer in self.layers[1:]:
+            layer.compile(output_shape)
+            output_shape = layer.output_shape
+        self.params = self.get_params()
+
+    def __call__(self, data: np.ndarray) -> np.ndarray:
+        """
+        Realiza una propagación hacia adelante
+
+        :param x: tensor de entrada
+        :return y: tensor de salida
+        """
+        for layer in self.layers:
+            data = layer(data)
+        return data
+    
+    def backward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Realiza una propagación hacia atrás
+
+        :param grad_output: gradientes de la propagación hacia adelante
+        :param data: tensor de entrada
+        :return param_grads: gradientes de los parámetros
+        """      
+
+        preds = self(x)
+        grad_output = self.loss.backward(y, preds)
+        for layer in reversed(self.layers):
+            grad_output = layer.backward(grad_output)
+        return self.get_grads()
+    
+    def get_params(self) -> list[np.ndarray]:
+        """
+        Obtiene los parámetros del modelo
+
+        :return params: lista de parámetros
+        """
+        params = []
+        for layer in self.layers:
+            params.extend(layer.get_params())
+        return params
+
+    def get_grads(self) -> list[np.ndarray]:
+        """
+        Obtiene los gradientes de los parámetros del modelo
+
+        :return grads: lista de gradientes
+        """
+        grads = []
+        for layer in self.layers:
+            grads.extend(layer.get_grads())
+        return grads
+    
+    def update_params(self, param_grads: list[np.ndarray]) -> None:
+        """
+        Actualiza los parámetros del modelo
+
+        :param param_grads: gradientes de los parámetros	
+        """
+        self.params = self.optimizer(self.params, param_grads)
+        i = 0
+        for layer in self.layers:
+            if hasattr(layer, 'weights') and hasattr(layer, 'bias'):
+                layer.weights = self.params[i]
+                if hasattr(layer, 'recurrent_weights'):
+                    layer.recurrect_weights = self.params[i+1]
+                    layer.bias = self.params[i+2]
+                    i += 3
+                else:
+                    layer.bias = self.params[i+1]
+                    i += 2
 
     def log(self, text: str) -> None:
         """
@@ -57,9 +152,8 @@ class NeuralNetwork:
                 param[...] = file[f'param_{i}'][...]
                 self.params[i] = param
 
-
 class ConvolutionalNeuralNetwork(NeuralNetwork):
-    def __init__(self, input_shape: tuple[int, ...], layers: list[Layer], optimizer: Optimizer, loss: Loss, metrics: any, verbose: bool=True) -> None:
+    def __init__(self, input_shape: tuple[int, ...], layers: list[Layer], optimizer: Optimizer, loss: Loss, metrics: any, verbose: bool=False) -> None:
         """
         Constructor de un modelo de red neuronal
 
@@ -71,98 +165,6 @@ class ConvolutionalNeuralNetwork(NeuralNetwork):
         :param verbose: indica si se debe mostrar mensajes de progreso
         """
         super().__init__(input_shape, layers, optimizer, loss, metrics, verbose)
-    
-    def summary(self) -> None:
-        """
-        Muestra un resumen del modelo
-        """
-        parameters = sum(param.size for param in self.get_params())
-        print(f'Modelo de red neuronal con {len(self.layers) + 1} capas:')
-        print(f'- input: Entrada - {self.input_shape}')
-        for layer in self.layers:
-            if hasattr(layer, 'weights') and hasattr(layer, 'bias'):
-                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape} - {layer.weights.size + layer.bias.size} params')
-                print(f'    Pesos: ', layer.weights.shape)
-                print(f'    Bias: ', layer.bias.shape)
-            else:
-                print(f'- {layer.name}: {layer.__class__.__name__} - {layer.output_shape}')
-        print(f'Optimizador: {self.optimizer.__class__.__name__} - Learning rate: {self.optimizer.learning_rate}')
-        print(f'Función de pérdida: {self.loss.__class__.__name__}')        
-        print(f'Función de cálculo de métricas: {self.metrics.__name__}')
-        print(f'Cantidad de parámetros: {parameters}')
-
-    def compile(self) -> None:
-        """
-        Compila el modelo.
-        """
-        self.layers[0].compile(self.input_shape)
-        output_shape = self.layers[0].output_shape
-        for layer in self.layers[1:]:
-            layer.compile(output_shape)
-            output_shape = layer.output_shape
-        self.params = self.get_params()
-
-    def get_params(self) -> list[np.ndarray]:
-        """
-        Obtiene los parámetros del modelo
-
-        :return params: lista de parámetros
-        """
-        params = []
-        for layer in self.layers:
-            params.extend(layer.get_params())
-        return params
-
-    def get_grads(self) -> list[np.ndarray]:
-        """
-        Obtiene los gradientes de los parámetros del modelo
-
-        :return grads: lista de gradientes
-        """
-        grads = []
-        for layer in self.layers:
-            grads.extend(layer.get_grads())
-        return grads
-
-    def __call__(self, data: np.ndarray) -> np.ndarray:
-        """
-        Realiza una propagación hacia adelante
-
-        :param x: tensor de entrada
-        :return y: tensor de salida
-        """
-        for layer in self.layers:
-            data = layer(data)
-        return data
-    
-    def _backward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """
-        Realiza una propagación hacia atrás
-
-        :param grad_output: gradientes de la propagación hacia adelante
-        :param data: tensor de entrada
-        :return param_grads: gradientes de los parámetros
-        """      
-
-        preds = self(x)
-        grad_output = self.loss.backward(y, preds)
-        for layer in reversed(self.layers):
-            grad_output = layer.backward(grad_output)
-        return self.get_grads()
-        
-    def _update_params(self, param_grads: list[np.ndarray]) -> None:
-        """
-        Actualiza los parámetros del modelo
-
-        :param param_grads: gradientes de los parámetros	
-        """
-        self.params = self.optimizer(self.params, param_grads)
-        i = 0
-        for layer in self.layers:
-            if hasattr(layer, 'weights') and hasattr(layer, 'bias'):
-                layer.weights = self.params[i]
-                layer.bias = self.params[i+1]
-                i += 2
 
     def train(self, train_data: np.ndarray, train_labels: np.ndarray, validation_data: tuple[np.ndarray, np.ndarray]=(None, None), epochs: int=10, batch_size: int=32) -> None:
         """
@@ -191,12 +193,12 @@ class ConvolutionalNeuralNetwork(NeuralNetwork):
                 loss_history.append(loss)
                 metric_history.append(metric)
 
-                grads = self._backward(x_batch, y_batch)
+                grads = self.backward(x_batch, y_batch)
                 param_grads = [
                     np.where(abs(grad / x_batch.shape[0]) < 1e-07, 0, grad / x_batch.shape[0]) 
                     for grad in grads
                 ]
-                self._update_params(param_grads)
+                self.update_params(param_grads)
                 
             self.log(f'Epoch {epoch + 1}/{epochs} - Loss: {np.mean(loss_history):.4f} - {metric_name}: {np.mean(metric_history)}')
             train_global_loss.append(np.mean(loss_history))
@@ -236,3 +238,72 @@ class ConvolutionalNeuralNetwork(NeuralNetwork):
         metric = self.metrics(test_labels, pred)
 
         return loss, metric
+
+    # TODO: Verificar funcionamiento con distintos modelos y imagenes.
+    def get_gradcam(self, data: np.ndarray, alpha: float=1.0) -> np.ndarray:
+        """
+        Obtiene el Grad-CAM de una imagen
+
+        :param data: matriz de entrada
+        :param alpha: factor de regularización
+        :return gradcam: matriz de gradiente CAM
+        """
+        x = data
+        if len(x.shape) == 3: x = np.expand_dims(x, axis=0)
+
+        pred = self(x)
+        num_classes = pred.shape[1]
+        class_idx = np.argmax(pred, axis=1)
+
+        layers = self.layers
+        last_conv_layer = [layer for layer in layers if isinstance(layer, Conv2D)][-1]
+        for layer in layers:
+            x = layer(x)
+            if layer == last_conv_layer: break
+        feature_maps = x
+
+        grad_output = np.zeros(num_classes)
+        grad_output[class_idx] = 1
+
+        dense_weights_1 = self.layers[-1].weights
+        dense_weights_2 = self.layers[-2].weights
+        grad_dense_2 = np.dot(grad_output, dense_weights_1.T)
+        grad_dense_1 = np.dot(grad_dense_2, dense_weights_2.T)
+
+        grad_pooled = grad_dense_1.reshape(32, 1, 13, 13)
+        feature_maps = feature_maps.transpose(1, 0, 2, 3)
+
+        maxpool = self.layers[1].copy()
+        maxpool.forward_data = feature_maps
+        grad_feature_maps = maxpool.backward(grad_pooled)
+        grad_feature_maps = np.mean(grad_feature_maps, axis=(1, 2, 3))
+
+        heatmap = feature_maps.transpose(1, 0, 2, 3)[0].T @ grad_feature_maps
+        heatmap = heatmap.T
+        heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+        heatmap = np.uint8(255 * heatmap)
+
+        jet = plt.get_cmap('viridis')
+        jet_colors = jet(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+        jet_heatmap = Image.fromarray((jet_heatmap * 255).astype(np.uint8))
+        jet_heatmap = jet_heatmap.resize((28, 28), resample=Image.NEAREST)
+        jet_heatmap = np.array(jet_heatmap, dtype=np.float32) / 255.0
+
+        img = data[0].reshape(28, 28, 1) / 255
+        gradcam = jet_heatmap * alpha + img
+        return gradcam
+
+class RecurrentNeuralNetwork(NeuralNetwork):
+    def __init__(self, input_shape: tuple[int, ...], layers: list[Layer], optimizer: Optimizer, loss: Loss, metrics: any, verbose: bool=False) -> None:
+        """
+        Constructor de un modelo de red neuronal
+
+        :param input_shape: forma de la entrada
+        :param layers: capas de la red
+        :param optimizer: método de optimización
+        :param loss: función de pérdida
+        :param metrics: función de cálculo de métricas
+        :param verbose: indica si se debe mostrar mensajes de progreso
+        """
+        super().__init__(input_shape, layers, optimizer, loss, metrics, verbose)
